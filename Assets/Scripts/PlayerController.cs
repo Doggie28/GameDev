940b9b0511d6f4822f40cb038c7903b9f7cda958
Juliet <juliet28garcia@gmail.com>
2020-08-13 15:51:10 -0500

    Code and Animation Fixes

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{   
+
+
+    public Animator anim;
+
+    // Start is called before the first frame update
+    //Will be called once
+    void Start()
+    {
+        
+    }
+
+    public void myFunction()
+    {
+
+    }
+
+
+   // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("Jump");
+
+        }
+    }
+   
+
+}//EndClass
